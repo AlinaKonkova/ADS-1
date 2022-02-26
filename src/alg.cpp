5eae57dabeb8dc98a79ba0fa78ec467cbbe2818c
Alina Konkova <99170615+AlinaKonkova@users.noreply.github.com>
2022-02-26 14:15:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,46 +5,46 @@
 
 
 bool checkPrime(uint64_t value) {
-  if (value <= 2) {
+    if (value <= 2) {
+        return true;
+    }
+    for (int i = 2; i < value; i++) {
+        if (value % i == 0) 
+            return false;
+    }
     return true;
-  }
-  for (int i = 2; i < value; i++) {
-    if (value % i == 0) 
-      return false;
-  }
-  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  int count = 0;
-  int num = 2;
-  while (true) {
-    if (checkPrime(num)) {
-      count++;
+    int count = 0;
+    int num = 2;
+    while (true) {
+        if (checkPrime(num)) {
+            count++;
+        }
+        if (count == n) {
+            return num;
+        }
+        num++;
     }
-    if (count == n) {
-      return num;
-    }
-  num++;
-  }
-  return num;
+    return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  while (true) {
-    value++;
-    if (checkPrime(value))
-      return value;
-  }
-  return value;
+    while (true) {
+        value++;
+        if (checkPrime(value))
+            return value;
+    }
+    return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  uint64_t sum = 0;
-  for (uint64_t num = 2; num < hbound; num++) {
-      if (checkPrime(num) == true) {
-        sum = sum + num;
-      }
-  }
-  return sum;
+    uint64_t sum = 0;
+    for (uint64_t num = 2; num < hbound; num++) {
+        if (checkPrime(num) == true) {
+            sum = sum + num;
+        }
+    }
+    return sum;
 }
